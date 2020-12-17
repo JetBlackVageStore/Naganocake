c3280f4161dd7b34baf3f3eab715dbf90c97490b
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-12-17 09:12:11 +0900

    DB Customers add is_active & views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_232858) do
+ActiveRecord::Schema.define(version: 2020_12_17_000846) do
 
   create_table "add_deliveries", force: :cascade do |t|
     t.integer "customer_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_232858) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_active"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
