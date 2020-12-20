90c757d81b98fd083dfeaba8f0a301db7fbce9f1
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-12-20 19:56:28 +0900

    order_item 修正

--- app/controllers/admin/orders_items_controller.rb
+++ app/controllers/admin/orders_items_controller.rb
@@ -2,13 +2,13 @@ class Admin::OrdersItemsController < ApplicationController
   def update
     order_item = OrderItem.find(params[:id])
     order = Order.find_by(id: order_item.order_id)
-    order_products = order.order_items
+    order_item = order.order_items
     order_item.update(order_item_params)
 
 
     if order_item.prod_status == "製作中"
       order.update(status: "製作中")
-    elsif order_products.all?{|op| op.prod_status == "製作完了"}
+    elsif order_items.all?{|op| op.prod_status == "製作完了"}
       order.update(status: "発送準備中")
     end
     redirect_to admin_order_path(order_item.order)
