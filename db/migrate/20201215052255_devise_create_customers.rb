7f7ca023d3a325089f044a3e73971ed1c7daaa6f
EC2 Default User <ec2-user@ip-172-31-68-201.ec2.internal>
2020-12-26 08:26:10 +0000

    [schema]

--- db/migrate/20201215052255_devise_create_customers.rb
+++ db/migrate/20201215052255_devise_create_customers.rb
@@ -11,7 +11,7 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
 
       ## Recoverable
       t.string   :reset_password_token
-      t.datetime :reset_password_sent_atr
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_074601) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_atr"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "surname"
     t.string "firstname"
